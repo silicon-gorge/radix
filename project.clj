1565bb9204b1dad19aa0018577ef19dfaceeffed
Neil Prosser <neil.prosser@nokia.com>
2014-10-06 15:20:25 +0100

    Get ready for release

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject mixradio/radix "0.1.0"
+(defproject mixradio/radix "1.0.0-SNAPSHOT"
   :description "A Clojure library providing the root functionality for web services"
+  :license "https://github.com/mixradio/radix/blob/master/LICENSE"
   :url "https://github.com/mixradio/radix"
   :dependencies [[environ "1.0.0"]
                  [metrics-clojure "2.3.0"]
@@ -7,6 +8,4 @@
                  [metrics-clojure-jvm "2.3.0"]
                  [metrics-clojure-ring "2.3.0"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]]
-  :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars})
+                 [org.slf4j/jul-to-slf4j "1.7.7"]])
