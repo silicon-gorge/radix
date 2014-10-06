6a859d41f76a0c8e4c2f70a068ea84d1477339e8
Neil Prosser <neil.prosser@nokia.com>
2014-10-06 15:16:01 +0100

    Add in release settings

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject mixradio/radix "0.1.0-SNAPSHOT"
-  :description "Common setup for MixRadio Clojure services"
-  :url "http://github.brislabs.com/libraries/cljsetup"
+  :description "A Clojure library providing the root functionality for web services"
+  :url "https://github.com/mixradio/radix"
   :dependencies [[environ "1.0.0"]
                  [metrics-clojure "2.3.0"]
                  [metrics-clojure-graphite "2.3.0"]
                  [metrics-clojure-jvm "2.3.0"]
                  [metrics-clojure-ring "2.3.0"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]])
+                 [org.slf4j/jul-to-slf4j "1.7.7"]]
+  :plugins [[lein-release "1.0.5"]]
+  :lein-release {:deploy-via :clojars})
