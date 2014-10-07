f2496dbed5c858f8cf0dda91e0945b9a9bb7cc04
Neil Prosser <neil.prosser@nokia.com>
2014-10-07 14:51:45 +0100

    Tag hacks

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 lein vcs assert-committed
 lein change version leiningen.release/bump-version release
 lein vcs commit
-lein vcs tag
+git tag `cat project.clj | grep defproject | cut -d" " -f 3 | tr -d "\""` # I'm actually pretty happy with this
 lein deploy clojars
 lein change version leiningen.release/bump-version
 lein vcs commit
