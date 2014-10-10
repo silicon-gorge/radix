c2a8f14f3034e8c8c65d28815aab71276263cc8b
Matthew Daley <matthew.daley@nokia.com>
2014-10-10 15:50:47 +0100

    Introducted auto-reload flag to separate this from the production flag.

--- test/radix/reload_test.clj
+++ test/radix/reload_test.clj
@@ -2,28 +2,32 @@
   (:require [midje.sweet :refer :all]
             [ns-tracker.core :refer [ns-tracker]]
             [radix
-             [setup :refer [production?]]
+             [setup :refer [auto-reload?]]
              [reload :refer :all]]))
 
 (fact-group
- (fact "wrap-reload does nothing in production mode, just returning the handler passed in"
-       (binding [production? true]
+ (fact "wrap-reload does nothing when auto-reload is false, just returning the handler passed in"
+       (binding [auto-reload? false]
          (wrap-reload :handler) => :handler))
 
- (fact "wrap-reload does nothing in non-production mode when no namespaces need to be reloaded"
+ (fact "wrap-reload does nothing when auto-reload is not set"
+       (binding [auto-reload? nil]
+         (wrap-reload :handler) => :handler))
+
+ (fact "wrap-reload does nothing when auto-reload is true but no namespaces need to be reloaded"
        (against-background
         (ns-tracker ["src"]) => (fn [] nil))
-       (binding [production? false]
+       (binding [auto-reload? true]
          (let [handler (fn [r] r)
                wrapped-handler (wrap-reload handler)]
            (wrapped-handler :request) => :request)))
 
- (fact "wrap-reload performs reload in non-production mode when namespaces need to be reloaded"
+ (fact "wrap-reload performs reload when auto-reload is true and namespaces need to be reloaded"
        (against-background
         (ns-tracker ["src"]) => (fn [] #{"ns1" "ns2"})
         (require "ns1" :reload) => nil
         (require "ns2" :reload) => nil)
-       (binding [production? false]
+       (binding [auto-reload? true]
          (let [handler (fn [r] r)
                wrapped-handler (wrap-reload handler)]
            (wrapped-handler :request) => :request))))
