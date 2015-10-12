d2543ab6843dddfcb2e6b3529ea6afac633f8158
Luke Snape <ldjsnape@gmail.com>
2015-10-12 13:25:49 +0100

    Version 1.0.20-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mixradio/radix "1.0.19"
+(defproject mixradio/radix "1.0.20-SNAPSHOT"
   :description "A Clojure library providing the root functionality for web services"
   :license "https://github.com/mixradio/radix/blob/master/LICENSE"
   :url "https://github.com/mixradio/radix"
